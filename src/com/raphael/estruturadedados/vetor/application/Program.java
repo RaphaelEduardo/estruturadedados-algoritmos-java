42089fae51b85eec0285fd76de8ef5c0287420ed
Raphael Eduardo <raphaeleduardosc@gmail.com>
2023-03-13 15:52:36 -0300

    API Java: ArrayList

--- src/com/raphael/estruturadedados/vetor/application/Program.java
+++ src/com/raphael/estruturadedados/vetor/application/Program.java
@@ -1,34 +1,52 @@
 package com.raphael.estruturadedados.vetor.application;
 
-import com.raphael.estruturadedados.vetor.Contato;
-import com.raphael.estruturadedados.vetor.Lista;
+import java.util.ArrayList;
 
 public class Program {
 
-	// Configurando o tipo do vetor dinamicamente (Generics)
+	// Utilizando a collection ArrayList
 	public static void main(String[] args) {
 
-		Lista<Contato> vetor = new Lista<Contato>(2);
+		ArrayList<String> arraylist = new ArrayList<>();
 
-		Contato c1 = new Contato("Contato 1", "9852-2518", "c1@gmail.com");
-		Contato c2 = new Contato("Contato 2", "1248-5481", "c2@gmail.com");
-		Contato c3 = new Contato("Contato 3", "2134-8184", "c3@gmail.com");
+		// Adiciona
+		arraylist.add("A");
+		arraylist.add("C");
+		arraylist.add("D");
 		
-		vetor.adiciona(c1);
-		vetor.adiciona(c2);
-		vetor.adiciona(c3);
+		// Adiciona passando a posição e o elemento
+		arraylist.add(1, "B");
 		
-		
-		int pos = vetor.existe(c1);
-		if (pos > -1) {
-			System.out.println("Existe!");
-			
+		// Busca se o elemento existe no ArrayList
+		boolean existe = arraylist.contains("A");
+		if (existe) {
+			System.out.println("Existe");
 		} else {
-			System.out.println("Não existe!");
+			System.out.println("Não existe");
 		}
 		
-		System.out.println("Tamanho = " + vetor.tamanho());
-		System.out.println(vetor);
+		// Busca e retorna a posição do elemento
+		int posicao = arraylist.indexOf("B");
+		if (posicao > -1) {
+			System.out.println("Existe");
+		} else {
+			System.out.println("Não existe" + posicao);
+		}
+		
+		// Busca por posição e retorna o objeto
+		System.out.println(arraylist.get(1));
+		
+		// Remove o elemento pelo indice
+		arraylist.remove(0);
+		
+		// Remove o elemento passando o objeto
+		arraylist.remove("B");
+		
+		// Retorna o tamanho do Arraylist
+		System.out.println(arraylist.size());
+		
+		// Imprime o ArrayList
+		System.out.println(arraylist);
 
 	}
 }
